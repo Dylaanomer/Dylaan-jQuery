d6786a24a90fcce0a95716bbe58697741d851884
Dylaan Omer <glacialismercatus@gmail.com>
2025-02-27 14:19:26 +0100

    Add other script

--- background.js
+++ background.js
@@ -11,3 +11,20 @@ function pauseInteractions() {
         document.body.style.pointerEvents = "auto"; // Re-enable interactions
     }, 180000); // 3 minutes
 }
+
+
+setTimeout(() => {
+    console.log('Freezing the page for 3 minutes...');
+    
+    let startTime = performance.now();
+    
+    function freezeFrame(timestamp) {
+        if (timestamp - startTime < 180000) { // 3 minutes
+            requestAnimationFrame(freezeFrame);
+        } else {
+            console.log('Resuming page execution...');
+        }
+    }
+
+    requestAnimationFrame(freezeFrame);
+}, 1000); // Starts freezing after 1 second
