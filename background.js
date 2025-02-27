eeaa09e07d5d773ebf57ceb9a43c1a4256f9f71b
Dylaan Omer <glacialismercatus@gmail.com>
2025-02-27 14:29:32 +0100

    Stops Divi slider

--- background.js
+++ background.js
@@ -51,3 +51,22 @@ setTimeout(() => {
         console.log("Resuming JavaScript execution...");
     }, 180000); // 3 minutes
 }, 1000);
+
+//STOP DIVI SLIDER
+
+setTimeout(() => {
+    console.log("Stopping Divi Slider autoplay...");
+    
+    let slider = document.querySelector('.et_pb_slider_0');
+    
+    if (slider) {
+        let clone = slider.cloneNode(true);
+        slider.parentNode.replaceChild(clone, slider); // Replaces it with a static version
+        console.log("Slider frozen");
+        
+        setTimeout(() => {
+            console.log("Resuming slider...");
+            location.reload(); // Reloads the page after 3 minutes to resume autoplay
+        }, 180000); // 3-minute delay
+    }
+}, 1000);
